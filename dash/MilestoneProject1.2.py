f07cb7f6134c2a42cbef83389414a3d2dc069d62
PabloJRW <ramoswilkinspj28@gmail.com>
2023-05-24 21:22:42 -0500

    version1.3

--- dash/MilestoneProject1.2.py
+++ dash/MilestoneProject1.2.py
@@ -7,21 +7,23 @@ from datetime import datetime
 ticker_options = {'Tesla':'TSLA', 'Apple':'AAPL', 'Microsoft':'MSFT', 'Amazon':'AMZN',
                   'Google':'GOOGL', 'Netflix': 'NFLX', 'Meta':'META'}
 
+interval_options = ['1h','1d','1wk','1mo','3mo']
+
 #ticker_options = [{'label':l, 'value':v} for l, v in ticker_options.items()]
 
-app = Dash(__name__)  
+app = Dash(__name__, external_stylesheets=['styles.css'])  
 
 app.layout = html.Div([
-    html.H1(
+    html.H1(  
+        id='header',
         children='NASDAQ Stocks Dashboard', 
         style={"display": "flex", "justify-content": "center",
-               'paddingTop':'1px', 'margin':'10px 0px'}
+               'padding':'18px', 'margin':'1px 0px', 'backgroundColor':'#F7931A',
+               'color':'#00FF00'}
     ),
-    html.Hr(
-        style={'margin': '1px 0px'}
-    ),
+    
     html.Div([
-
+ 
     #### INICIO DIV - Contenedor de selección de opciones
     # Este DIV es el área de selección de opciones, 
     # uno de los n contenedores principales. 
@@ -37,23 +39,23 @@ app.layout = html.Div([
                 style={'margin':'1.5px 3px', 'paddingRight':'12px'}
             ),
             dcc.Checklist(
-                id='stock-picker',
+                id='stock-ticker-picker',
                 options=[{'label': l, 
-                          'value': v} for l, v in ticker_options.items()],
-                value=['TSLA'], 
+                          'value': v} for l,v in ticker_options.items()],
+                value=['AAPL'], 
                 inline=True,
-                style={'width':'300px', 
+                style={
                        'marginRight':'1px',
                        'paddingTop':'6px', 
                        'paddingRight':'12px',
                        'paddingLeft':'12px',
                        'display':'flex',
-                       'alignItems':'center',
-                       'justifyContent':'center'
+                       'alignItems':'start' 
                        }
-            )
+            )  
         ],
-        style={'width':'420px', 'borderRight':'1px solid grey'}
+        style={'width':'420px', 'height':'69px','marginRight':'6px', 
+               'border':'1px solid #D9D2D0', 'backgroundColor':'#F7F7F7'}
         ),
     ## -----------------------------------------
 
@@ -67,52 +69,84 @@ app.layout = html.Div([
                 id='date-picker',
                 min_date_allowed=datetime(2015,1,1),
                 max_date_allowed=datetime.today(),
-                start_date=datetime(2022,1,1),
+                start_date=datetime(2019,1,1),
                 end_date=datetime.today(),
                 style={'marginLeft':'24px',
-                       'height':'90px',
-                       'position':'absolute'}
+                       'height':'10px',
+                       }
             )
         ], 
-        style={'margin':'1px 1px', 'display':'inline-block', 
-               'borderRight':'1px solid grey', 'width':'321px'},
+        style={'width':'321px' ,'height':'69px', 'display':'inline-block', 
+               'marginRight':'6px', 'border':'1px solid #D9D2D0', 'backgroundColor':'#F7F7F7'},
+        ),
+
+    ### INICIO DIV - selector de intervalo 
+        html.Div([ 
+            html.H3(
+                children='Select interval',
+                style={'margin':'1.5px 3px', 'paddingRight':'12px'}
+            ),
+            dcc.Checklist(
+                id='interval-selector',
+                options=[{'label':i,
+                          'value':i} for i in interval_options],
+                value=['1d'], 
+                inline=True,
+                style={ 
+                       'marginRight':'1px',
+                       'paddingTop':'6px', 
+                       'paddingRight':'12px',
+                       'paddingLeft':'12px',
+                       'display':'flex',
+                       #'alignItems':'center' 
+                       }
+            )  
+        ],
+        style={'width':'420px', 'height':'69px','marginRight':'6px', 
+               'border':'1px solid #D9D2D0', 'backgroundColor':'#F7F7F7'}
         ),
     ],
-    style={'height':'72px', 'display':'flex'}
+    style={'height':'72px', 'display':'flex', 'backgroundColor':'F7F7F7',
+           'justifyContent':'start', 'marginButton':'12px '}
     ),
     ### FINAL DIV - contenedor de selección de opciones
     ### *********
-    html.Hr(),
+    
+    html.Hr(style={'marginTop':'0px'}),
 
     ### INICIO DIV - Contenedor del gráfico
     dcc.Graph(
         id='my-graph',
         figure={'data':[]}
-    )
+    ),
     ### FINAL DIV - Contenedor del gráfico
     
-    
-])
-
-
+], style={'margin':'0px 0px'})
 
 
 @app.callback(Output('my-graph', 'figure'),
-              [Input('stock-picker', 'value'),
+              [Input('stock-ticker-picker', 'value'),
                Input('date-picker', 'start_date'),
-               Input('date-picker', 'end_date')])
-def update_graph(stock_ticker, start_date, end_date):
+               Input('date-picker', 'end_date'),
+               Input('interval-selector', 'value')])
+def update_graph(stock_ticker, start_date, end_date, interval):
     start_date = datetime.strptime(start_date[:10], '%Y-%m-%d')
     end_date = datetime.strptime(end_date[:10], '%Y-%m-%d')
 
     traces = [] 
     if len(stock_ticker) > 1:
         for tick in stock_ticker:
-            df = yf.Ticker(tick).history(interval='1d',start=start_date, end=end_date)
-            traces.append({'x':df.index, 'y':df['Close'], 'name':tick})
+            df = yf.Ticker(tick).history(interval='1d', 
+                                         start=start_date, 
+                                         end=end_date)
+            traces.append({'x':df.index, 'y':df['Close'], 
+                           'name':tick})
     elif len(stock_ticker) == 1:
-        df = yf.Ticker(stock_ticker[0]).history(interval='1d',start=start_date, end=end_date)
-        traces.append({'x':df.index, 'y':df['Close']})
+        df = yf.Ticker(stock_ticker[0]).history(interval='1d',
+                                                start=start_date, 
+                                                end=end_date)
+        traces.append({'x':df.index, 'y':df['Close'], 
+                       'name':stock_ticker})
         
     fig = {'data':traces, 
            'layout':{'title':stock_ticker}
